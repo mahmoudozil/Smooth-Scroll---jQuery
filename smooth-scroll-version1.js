a6fd9bb7a91eb9c62162527ca69e6b571e6bf159
Zied Guesmi <Guesmy.Zied@gmail.com>
2017-10-28 04:24:39 +0100

    Update smooth-scroll-version1.js

--- smooth-scroll-version1.js
+++ smooth-scroll-version1.js
@@ -1,7 +1,19 @@
-$('nav').click(function () {
-  var $href = $(this).attr('href');
-  $('body').stop().animate({
-    scrollTop: $($href).offset().top
-  }, 1000);
-  return false;
-});
+<html>
+  <head></head>
+
+  <body>
+    <nav> my test nav </nav>
+  </body>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script>
+  $('nav').click(function () {
+    var $href = $(this).attr('href');
+    $('body').stop().animate({
+      scrollTop: $($href).offset().top
+    }, 1000);
+    return false;
+  });
+  </script>
+
+</html>
